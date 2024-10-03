4e191b6d2243322ffc89a2e36a896e630bcb4534
Bedirhan Saglam <bedirhansaglam270@gmail.com>
2024-10-04 00:40:39 +0300

    update anime detail event

--- lib/features/detail/presentation/bloc/anime_detail_event.dart
+++ lib/features/detail/presentation/bloc/anime_detail_event.dart
@@ -3,13 +3,13 @@ part of 'anime_detail_bloc.dart';
 @immutable
 abstract class AnimeDetailEvent extends Equatable {
   const AnimeDetailEvent();
-
-  @override
-  List<Object?> get props => [];
 }
 
 class FetchAnimeCharactersEvent extends AnimeDetailEvent {
   final int id;
 
   const FetchAnimeCharactersEvent(this.id);
+
+  @override
+  List<Object?> get props => [id];
 }
